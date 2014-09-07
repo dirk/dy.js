ae512f41163bc0cd4cb0089a34731aaebd215757
Dirk Gadsden <dirk@esherido.com>
2014-09-07 16:38:39 -0400

    Add philosophy to README and documentation to browser lib

--- README.md
+++ README.md
@@ -3,6 +3,14 @@
 
 We've put humans on the moon and rovers on Mars. So why the bloody hell is save-and-reload still acceptable as the cornerstone of our development workflow? dy.js is proposed methodology (specification?) and reference implementation for module-based dynamic code reloading in (initially) the browser.
 
+## Philosophy
+
+dy.js is designed to be simple, humanist software. As such, it's guiding philosophy is centered around understandability and ease of use:
+
+1. Simple module dependency injection
+2. Simple module reloading extension
+3. Code that JavaScript programmers of nearly all skill levels should be able to understand
+
 ### License
 
 Released under the New BSD License (1999); see `LICENSE` for details.
--- dy.js
+++ dy.js
@@ -1,17 +1,25 @@
+
+// This is the base browser library for dy.js. It is all that is needed in the
+// browser for modules written for dy.js to work.
+//
+// In development you'd probably also want to include the dy/ext/reload
+// extension to add dynamic reloading functionality. However, in production
+// this is the *only* code you need (plus a `dy.load()` somewhere) for your
+// dy.js-compatible code to work.
 (function (window) {
 
   var dy = function (name, dependencies, initializer) {
-    var self = dy.retrieveOrCreate(name)
+    var self = dy.retrieveOrCreateModule(name)
     // Ensure all the dependencies exist
     dependencies = dependencies.map(function (dep) {
-      return dy.retrieveOrCreate(dep)
+      return dy.retrieveOrCreateModule(dep)
     })
     initializer.apply(self, dependencies)
   }
   // Registry of all active modules
   dy.modules = {}
 
-  dy.retrieveOrCreate = function (name) {
+  dy.retrieveOrCreateModule = function (name) {
     if (dy.modules[name] === undefined) {
       // Set up new instance
       dy.modules[name] = {}
